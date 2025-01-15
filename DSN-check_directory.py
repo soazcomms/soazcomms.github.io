0d8a3b429c08f1ce279398c4e7ef60efa578c11e
soazcomms <soazcomms@darksky.org>
2025-01-15 11:38:43 -0700

    Update DSN-check_directory.py

--- DSN-check_directory.py
+++ DSN-check_directory.py
@@ -14,4 +14,4 @@
 import github
 g = github.Github("soazcomms", "RedCielo25")
 repo = g.get_user().get_repo( "soazcomms.github.io" )
-print repo.get_dir_contents("")
+print (repo.get_dir_contents(""))
