312bdb79a1d537f0310aae674d50225a7b611fb0
soazcomms <soazcomms@darksky.org>
2025-01-15 13:40:08 -0700

    Add GitHub workflow to run Python script

--- DSN-check_directory.py
+++ DSN-check_directory.py
@@ -1,17 +1,17 @@
-#import os
+import os
 
-#def check_directory(directory):
-#    try:
-#        files = os.listdir(directory)
-#        print(f"Files in '{directory}': {files}")
-#    except Exception as e:
-#        print(f"Error checking directory: {e}")
+def check_directory(directory):
+    try:
+        files = os.listdir(directory)
+        print(f"Files in '{directory}': {files}")
+    except Exception as e:
+        print(f"Error checking directory: {e}")
 
-# Example directory to check
-#directory_to_check = 'soazcomms.github.io/DSNdata/NEW'  # Replace with your directory path
-#check_directory(directory_to_check)
-
-import github
-g = github.Github("soazcomms", "RedCielo25")
-repo = g.get_user().get_repo( "soazcomms.github.io" )
-print (repo.get_dir_contents(""))
+# directory to check
+directory_to_check = '/home/runner/work/soazcomms/DSNdata/NEW'  # Replace with your directory path
+check_directory(directory_to_check)
+#
+#import github
+#g = github.Github("soazcomms", "RedCielo25")
+#repo = g.get_user().get_repo( "soazcomms.github.io" )
+#print (repo.get_dir_contents(""))
