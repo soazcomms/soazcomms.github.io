bdc120beb91815ceb2a219652f463477223461c5
Emilio Falco <soazcomms@darksky.org>
2025-01-18 16:07:02 -0700

    Update DSN_V02.py

--- DSN_V02.py
+++ DSN_V02.py
@@ -93,7 +93,7 @@ night=np.full(nentries,0)
 pd.options.mode.copy_on_write = True
 # influxDB options
 dficha='LKXtshm7XHpORxczh-ZHJ5JViRk1XDEZuj3YF2wMLHoWV4KafApd7yO6DgUCHp07f_VaRj8pIi8LvkGJSWJNZQ=='
-gficha='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'
+gficha='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'
 url='https://us-east-1-1.aws.cloud2.influxdata.com'
 bucket='DSNdata'
 org='DSN'
